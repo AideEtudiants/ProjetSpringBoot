e3ff2b68f012bf81e7ff135e7a9b39f86c9618e4
Emmanuela <kende.emmanuela@gmail.com>
2021-11-20 23:49:12 +0100

    getAll product : modification dans le WelcomePageController

--- src/main/java/projet/studenity/controller/WelcomePageController.java
+++ src/main/java/projet/studenity/controller/WelcomePageController.java
@@ -1,11 +1,10 @@
 package projet.studenity.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import projet.studenity.dao.ProductDao;
 import projet.studenity.model.Product;
+import projet.studenity.repository.ProductRepository;
 import projet.studenity.service.ProductService;
 import projet.studenity.service.WelcomePageService;
 
@@ -13,15 +12,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/acceuil")
+@CrossOrigin(origins = "http://localhost:4200")
 public class WelcomePageController {
     @Autowired
     ProductService productService;
     @Autowired
+    ProductDao productDao;
+    @Autowired
     WelcomePageService welcomePageService;
 
-    @GetMapping(value="/{name}")
+ /*   @GetMapping(value="/{name}")
     public List<Product> findProductByName(@PathVariable("name") String name){
         return productService.findProductByName(name);
+    }*/
+
+    @GetMapping(value="/acceuil")
+    public List<Product> getProduct(){
+        return productDao.getAll();
     }
 
 }

fb774d8dd6916262d0ff388a68767b15c4464269
Emmanuela <kende.emmanuela@gmail.com>
2021-11-28 22:24:33 +0100

    essai liste : tester avec la liste dans le HelloController pour l'instant ( pour ceux dont l'url /homepage fonctionne, veillez modifiez le fichier RechercheServive en angular )

--- src/main/java/projet/studenity/controller/HelloController.java
+++ src/main/java/projet/studenity/controller/HelloController.java
@@ -1,13 +1,22 @@
 package projet.studenity.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import projet.studenity.dao.ProductDao;
+import projet.studenity.model.Product;
+import projet.studenity.repository.ProductRepository;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 public class HelloController {
 
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    ProductDao productDao;
+    String list[] = {"Hello word", "Emma", "Aniesse"};
     @RequestMapping("hello")
     // public List<Hello> getMessage(){
     //     List<Hello> messageList = new ArrayList<>();
@@ -16,12 +25,12 @@ public class HelloController {
     //     return messageList;
 
     // }
-       public String getMessage(){
+       public String[] getMessage(){
         // Hello m1= new Hello("hello");
         // m1.getMessage()
-        return "Hello word";
 
+        return list;
     }
-    
-    
+
+
 }

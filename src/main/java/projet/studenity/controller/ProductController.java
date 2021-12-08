16328ee1eec7380eb6399896e4797f08516ac7bf
AKKOURA <64647807+AKKOURA@users.noreply.github.com>
2021-12-08 14:55:05 +0100

    ProductPage

--- src/main/java/projet/studenity/controller/ProductController.java
+++ src/main/java/projet/studenity/controller/ProductController.java
@@ -3,19 +3,19 @@ package projet.studenity.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import projet.studenity.dao.ProductDao;
-import projet.studenity.dao.UserDao;
 import projet.studenity.model.Product;
-import projet.studenity.model.User;
+import projet.studenity.repository.ProductRepository;
 import projet.studenity.service.ProductService;
-import projet.studenity.service.UserService;
 
 @RestController
-@RequestMapping(value="/product")
+@CrossOrigin(origins = "http://localhost:4200")
+// @RequestMapping(value="/product")
 public class ProductController {
 
 	@Autowired
@@ -23,10 +23,12 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
+	@Autowired
+	private ProductRepository productRepository;
   
-	@GetMapping(value="/all")
+	@GetMapping(value="product/all")
 	public List<Product> getAllProducts() {
-		return productService.getProducts();
+		return (List<Product>) productRepository.findAll();
 	}
 
 	@GetMapping(value="/{id}")
@@ -35,9 +37,10 @@ public class ProductController {
 	}
 
 	@GetMapping(value="/{name}")
-	public String findProductByName(@PathVariable("name") String name) {
+	public List<Product> findProductByName(@PathVariable("name") String name) {
 		List<Product> listProduct = productService.findProductByName(name);
-		return listProduct.toString();
+		return listProduct
+
 	}
 
 	@GetMapping(value="/delete/{id}") //Attendre Product de FE
